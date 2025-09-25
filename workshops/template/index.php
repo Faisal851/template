b791f7362281fca9887f915e45a15345c67578da
Faisal Ahmadi <176859@student.talland.nl>
2025-09-25 13:07:14 +0200

    Uitwerking van de exercise

---
+++ workshops/template/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Mijn Website</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <h1>Hallo wereld!</h1>
+</body>
+</html>
